a7ca5f758304bf117def905b725ef44e0a2c6e09
Ilya Zolotov <zolotovil@mail.ru>
2024-07-24 09:48:54 +0300

    Deleted extra slash from url

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -3,7 +3,7 @@ import requests
 
 class TestOrderList:
     def test_get_order_list(self):
-        response = requests.get("https://qa-scooter.praktikum-services.ru//api/v1/orders")
+        response = requests.get("https://qa-scooter.praktikum-services.ru/api/v1/orders")
         order_data = response.json()
         order_list = order_data['orders']
         assert isinstance(order_list, list)
